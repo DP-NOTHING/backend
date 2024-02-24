73d2a11525c391b33ee08e892bb15c3504442c09
Deep Patel <deep1907rp@gmail.com>
2024-02-24 14:54:04 +0530

    fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var admin = require("firebase-admin");
 admin.initializeApp({
   credential: admin.credential.cert({
     type: process.env.type,
-    project_id: process.env.project_id,
+    projectId: process.env.project_id,
     private_key_id: process.env.private_key_id,
     private_key: process.env.private_key,
     client_email: process.env.client_email,
